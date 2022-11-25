98431d5a56fe39ed3fa4a2e6aae7db0bf636842e
Shoaib Ali Ajaib <ajxa@pm.me>
2022-11-25 18:15:51 +0000

    Add missing fields function into the main check_dictionary function

--- R/check_dictionary.R
+++ R/check_dictionary.R
@@ -4,13 +4,15 @@
 #' rows which invalidate any checks.
 #'
 #' @param dictionary a list tables corresponding to individual data dictionary tables
+#' @param reference a list of data.frame containing a column of reference values to
+#'                  compare against the dictionary tables
 #' @param min_nchars the minimum number length of characters required (default = 2)
 #'                   for the display_name, display_name_label and field_description
 #'                   fields
 #' @return list comprising of cleaned data dictionary tables and information about
 #'         various column level checks
 #' @export
-check_dictionary <- function(dictionary, min_nchars = 2){
+check_dictionary <- function(dictionary, reference, min_nchars = 2){
 
     checks <- list()
 
@@ -40,12 +42,44 @@ check_dictionary <- function(dictionary, min_nchars = 2){
         .x = dictionary, ~check_value_lengths(.x, values))
 
 
+    # summarise the checks and flag problematic tables
+    checks <- purrr::imap(checks, ~{
+
+        flagged_check <- which(unlist(.x))
+
+        tabs  <- switch(.y,
+                        data_type = paste(rep("\t",3),collapse =""),
+                        variable_type = paste(rep("\t",3),collapse =""),
+                        display_name_len = paste(rep("\t",2),collapse =""),
+                        display_label_len = paste(rep("\t",2),collapse =""),
+                        display_field_desc_len = paste(rep("\t",2),collapse =""),
+                        values = paste(rep("\t",4),collapse ="")
+                        )
+
+
+        if(length(flagged_check) == 0){
+
+            cat(glue::glue(crayon::blue$bold("\n{.y}{tabs}PASS")))
+            return(TRUE)
+
+            }else{
+
+                cat(glue::glue(crayon::red$bold("\n{.y}{tabs}FAIL")))
+                return(names(.x[flagged_check]))
+            }
+
+    })
+
+
     checks$unique_units <- purrr::map(
         .x = dictionary, ~get_all_unique_units(.x, units))
 
 
     checks$all_unique_units <- unique(unlist(checks$unique_units))
 
+
+    checks$missing_fields <- check_missing_fields(dictionary = dictionary,
+                                                  reference = reference)
 
     return(checks)
 
@@ -125,33 +159,3 @@ check_value_lengths <- function(dictionary, field){
 }
 
 
-
-# summarise_checks <- function(checklist){
-#
-#     simple_checks <- checklist[-grep("units", names(checklist))]
-#
-#     descriptive_checks <- checklist[grep("units", names(checklist))]
-#
-#     iwalk(simple_checks, ~{
-#
-#         if(all(unlist(.x) == 1)){
-#
-#             message(glue::glue(crayon::green("\n{.y} ALL good!")))
-#
-#         }else message(glue::glue(crayon::yellow("\n{.y} need checking!")))
-#     })
-#
-#
-#     unique_units <- sort(unique(unlist(descriptive_checks)))
-#
-#     unique_units <- paste0("\n","\t",1:length(unique_units), "\t", unique_units)
-#
-#     if(length(unique_units) >= 1){
-#
-#         message(glue::glue(crayon::blue("\nunique units:")))
-#         message(glue::glue(crayon::blue("\t\t{unique_units}")))
-#
-#     }else message(glue::glue(crayon::green("no unique units!")))
-#
-# }
-#
---
+++ R/missing_fields.R
@@ -0,0 +1,83 @@
+#' Check missing fields
+#'
+#' Compare the dictionary fields against a reference to identify fields which are
+#' missing in the dictionary
+#'
+#' @param dictionary a list of data.frames corresponding to an individual data dictionary tables
+#' @param reference a list of character vectors corresponding to field names
+#'                  which will be compared with those in each dictionary table
+#' @return either a logical when no missing fields were detected or a list characters
+#'         corresponding to fields missing per each specific table
+check_missing_fields <- function(dictionary, reference){
+
+    stopifnot("dictionary is not a list" = is.list(dictionary))
+    stopifnot("dictionary is not of length >= 1" = length(dictionary) >= 1)
+    stopifnot("dictionary table names not detected"  =!is.null(names(dictionary)))
+    stopifnot("reference is not a list" = is.list(reference))
+    stopifnot("reference is not of length >= 1" = length(reference) >= 1)
+    stopifnot("reference table names not detected" = !is.null(names(reference)))
+    stopifnot("dictionary and refernce have different lengthst" = length(dictionary) == length(reference))
+    stopifnot("dictionary table names differ from those in the refernce" = all(names(reference) %in% names(dictionary)))
+
+
+    # Ensure the order of both lists is the same
+    reference <- reference[match(names(dictionary), names(reference))]
+
+    outlist <- purrr::map2(.x = dictionary, .y = reference,
+                           ~missing_tbl_fields(.x, .y, display_name))
+
+    outlist <- purrr::flatten(outlist)
+
+
+    tabs <- paste(rep("\t",3),collapse ="")
+
+    if(length(outlist) == 0){
+
+        cat(glue::glue(crayon::blue$bold("\n\nmissing_fields{tabs}NO")))
+
+        return(FALSE)
+
+        }else{
+
+            cat(glue::glue(crayon::red$bold("\n\nmissing_fields{tabs}YES")))
+            return(outlist)
+
+    }
+
+}
+
+
+
+#' Compare fields with against a reference
+#'
+#' Compare fields against a reference to identify  fields which are
+#' missing in the dictionary table
+#'
+#' @param dictionary_tbl a data.frame corresponding to an individual data dictionary table
+#' @param reference_tbl a data.frame containing a column of reference values to compare against
+#' @param field the name of the field to compare in both the reference and dictionary
+#' @return logical
+missing_tbl_fields <- function(dictionary_tbl, reference_tbl, field){
+
+    dictionary_unique_fields <-  dictionary_tbl %>%
+        dplyr::select({{field}}) %>%
+        tidyr::drop_na() %>%
+        dplyr::distinct() %>%
+        dplyr::mutate(dplyr::across({{field}}, tolower)) %>%
+        dplyr::pull()
+
+    reference_unique_fields <- reference_tbl %>%
+        dplyr::select({{field}}) %>%
+        tidyr::drop_na() %>%
+        dplyr::distinct() %>%
+        dplyr::mutate(dplyr::across({{field}}, tolower)) %>%
+        dplyr::pull()
+
+
+    missing_feilds <- !reference_unique_fields %in% dictionary_unique_fields
+
+    if(any(missing_feilds)) reference_unique_fields[which(missing_feilds)] else NULL
+
+}
+
+
