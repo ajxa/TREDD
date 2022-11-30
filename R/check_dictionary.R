e2695d064a56406b2a14418d65b42ca0095da97f
Shoaib Ali Ajaib <ajxa@pm.me>
2022-11-30 14:04:20 +0000

    Fixed cli symbol reference error

--- R/check_dictionary.R
+++ R/check_dictionary.R
@@ -53,7 +53,7 @@ check_dictionary <- function(dictionary, reference, min_nchars = 2){
 
         if(length(flagged_check) == 0){
 
-            cli::cat_line(cli::col_green(symbol$tick),
+            cli::cat_line(cli::col_green(cli::symbol$tick),
                      cli::col_grey(" checking "),
                      cli::col_grey({check_out_name}),
                      cli::col_white(" ...")
@@ -63,7 +63,7 @@ check_dictionary <- function(dictionary, reference, min_nchars = 2){
 
             }else{
 
-                cli::cat_line(cli::col_red(symbol$cross),
+                cli::cat_line(cli::col_red(cli::symbol$cross),
                          cli::col_grey(" checking "),
                          cli::col_grey({check_out_name}),
                          cli::col_white(" ...")
