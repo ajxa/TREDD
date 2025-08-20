5c44fc75fa1682bca7c2b020811b58b08f7b5ac1
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2025-08-20 13:27:57 +0000

    added imperial pelvic to customer_lookup

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -54,7 +54,8 @@ customer_lookup = list(
     queenmary = "749612_d9m1v",
     harveywalsh = "596002_v3n9j",
     neqos = "10328_s0h5j",
-    imperialesc = "754175_w2z4t"
+    imperialesc = "754175_w2z4t",
+    imperialpelvic = "776195_h7w4d",
 )
 
 
